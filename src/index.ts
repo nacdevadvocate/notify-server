f9ff9f29ebc366bf649ef425f11cfb4582808478
Eyvazov Farid <farid.eyvazov@nokia.com>
2024-07-04 18:10:49 +0200

    Basic notification server created

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(morgan('tiny'));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -21,6 +22,44 @@ app.get("/", (req: Request, res: Response) => {
     console.log(message)
     res.status(200).json({ message });
 });
+
+
+app.post('/notifications', async (req: Request, res: Response) => {
+    try {
+        const apiKey = req.headers['x-api-key'] as string;
+
+        if (!apiKey) {
+            return res.status(400).json({ error: 'API key missing' });
+        }
+
+        console.log(apiKey);
+        const authToken = apiKey.split(' ')[0];
+
+        // Checking if the Bearer Token matches the provided token
+        if (authToken !== 'nacauth') {
+            return res.status(401).json({ error: 'Invalid Bearer Token' });
+        }
+
+        // Handling the notification payload asynchronously (simulated async operation)
+        console.log("show the device");
+        console.log(req.body?.event?.eventDetail?.device);
+
+        if (req.body?.event?.eventDetail?.deviceStatus === "REACHABLE") {
+            console.log("Device is available");
+            console.log(req.body?.event?.eventDetail?.device);
+        } else if (req.body?.event?.eventDetail?.deviceStatus === "UNREACHABLE") {
+            console.log("Device is not available");
+            console.log(req.body?.event?.eventDetail?.device);
+        }
+
+        // Sending a response
+        res.status(200).json({ message: 'Notification handled successfully' });
+    } catch (error) {
+        console.error('Error handling notification:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 
 
 // If API doesn't exsist
