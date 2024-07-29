3f16c3ced3448d153da8a473b3135170c3a85179
Eyvazov Farid <farid.eyvazov@nokia.com>
2024-07-29 11:39:33 +0200

    Merged and 2 more routes added

--- src/index.ts
+++ src/index.ts
@@ -27,36 +27,69 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 
-const attachments = [
-    {
-        fallback: "This is a message with JSON data",
-        color: "#36a64f",
-        pretext: "NaC Notifications",
-        author_name: "NaC",
-        author_link: "https://networkascode.nokia.io/",
-        author_icon: "https://networkascode.nokia.io/static-assets/default/dark-logo-1abf47ae-2af8-497d-9a8c-98a363044d5f-83c1f9f5-e37d-4802-9961-e7462ecc5e88.png",
-        title: "NaC API Documentation",
-        title_link: "https://developer.networkascode.nokia.io/",
-        text: "This is the main text of the attachment.",
-        fields: [
-            {
-                title: "Priority",
-                value: "High",
-                short: false
-            },
-            {
-                title: "Status",
-                value: "Completed",
-                short: false
-            }
-        ],
-        image_url: "hhttps://networkascode.nokia.io/static-assets/default/dark-logo-1abf47ae-2af8-497d-9a8c-98a363044d5f-83c1f9f5-e37d-4802-9961-e7462ecc5e88.png",
-        thumb_url: "https://networkascode.nokia.io/static-assets/default/dark-logo-1abf47ae-2af8-497d-9a8c-98a363044d5f-83c1f9f5-e37d-4802-9961-e7462ecc5e88.png"
-    }
-];
-
 
 app.post('/notifications', async (req: Request, res: Response) => {
+    try {
+        // Handling the notification payload asynchronously (simulated async operation)
+        console.log("show the device");
+        console.log(req.body?.event?.eventDetail?.device);
+
+        if (req.body?.event?.eventDetail?.deviceStatus === "REACHABLE") {
+            console.log("Device is available");
+            console.log(req.body?.event?.eventDetail?.device);
+
+        } else if (req.body?.event?.eventDetail?.deviceStatus === "UNREACHABLE") {
+            console.log("Device is not available");
+            console.log(req.body?.event?.eventDetail?.device);
+        }
+
+        // Sending a response
+        res.status(200).json({ message: 'Notification handled successfully' });
+    } catch (error) {
+        console.error('Error handling notification:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+app.post('/notifications-auth', async (req: Request, res: Response) => {
+    try {
+        const apiKey = req.headers['x-api-key'] as string;
+
+        if (!apiKey) {
+            return res.status(400).json({ error: 'API key missing' });
+        }
+
+        console.log(apiKey);
+        const authToken = apiKey.split(' ')[0];
+
+        // Checking if the Bearer Token matches the provided token
+        if (authToken !== 'nacauth') {
+            return res.status(401).json({ error: 'Invalid Bearer Token' });
+        }
+
+        // Handling the notification payload asynchronously (simulated async operation)
+        console.log("show the device");
+        console.log(req.body?.event?.eventDetail?.device);
+
+        if (req.body?.event?.eventDetail?.deviceStatus === "REACHABLE") {
+            console.log("Device is available");
+            console.log(req.body?.event?.eventDetail?.device);
+
+        } else if (req.body?.event?.eventDetail?.deviceStatus === "UNREACHABLE") {
+            console.log("Device is not available");
+            console.log(req.body?.event?.eventDetail?.device);
+        }
+
+        // Sending a response
+        res.status(200).json({ message: 'Notification handled successfully' });
+    } catch (error) {
+        console.error('Error handling notification:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/notifications-slack', async (req: Request, res: Response) => {
     try {
         console.log(req.body)
 
